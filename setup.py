ae11eec2f42a35994d7fd277b555a17b546e8782
Nico <nico@autoskope.de>
2026-01-21 14:55:16 +0100

    Add type hints support and licensing
      - Add py.typed for PEP 561 compliance
      - Add Apache 2.0 LICENSE file
      - Bump version to 1.3.0
      - Include py.typed in package distribution

--- setup.py
+++ setup.py
@@ -4,19 +4,24 @@ from setuptools import find_packages, setup
 
 setup(
     name="autoskope_client",
-    version="1.2.0",
+    version="1.3.0",
     description="Python client library for the Autoskope API.",
     author="Nico Liebeskind",
     author_email="nico@autoskope.de",
     url="https://github.com/mcisk/autoskope_client",
     packages=find_packages(),
+    package_data={
+        "autoskope_client": ["py.typed"],
+    },
     install_requires=[
         "aiohttp>=3.8.0",
     ],
     classifiers=[
         "Programming Language :: Python :: 3",
-        "License :: OSI Approved :: MIT License",
+        "License :: OSI Approved :: Apache Software License",
         "Operating System :: OS Independent",
+        "Typing :: Typed",
     ],
     python_requires=">=3.8",
+    license="Apache-2.0",
 )
